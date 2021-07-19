6922daa3e13c236dcce99589e3d292d7ea8c33e7
“Ricky <grickyh@gmail.com>
2021-07-19 06:49:18 +0100

    routes&vies

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
+// import useful node libraries
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+// import modules from routes directory for handling URL routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+// create express app object
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// request handling chain
+// add middleware libraries into request handling chain
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+// add code from routes modules to request handling chain
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -37,5 +40,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+// export && allow import by /bin/wwww
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
+// load express && express.router object
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+// route callback will executed when url /users is recieved
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+// /users/cool return Daisy so cool
+router.get('/cool', (req, res) => {
+  res.send('Daisy so cool')
+})
+// export && for import by app.js
 module.exports = router;
