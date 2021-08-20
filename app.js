4d7ecd4f9a284751797fd9149c691a966d5c9fc7
“Ricky <grickyh@gmail.com>
2021-08-20 07:41:31 +0100

    controller template

--- app.js
+++ app.js
@@ -4,17 +4,21 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+
 // import modules from routes directory for handling URL routes
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
+
 // create express app object
 let app = express();
+
 // Set up Connection to MongoDB
 let mongoose = require('mongoose');
 let mongDB = 'mongodb+srv://rickscode:Pollobibi2441@firstcluster.mqlg7.mongodb.net/local_library?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -26,6 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 // add code from routes modules to request handling chain
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
